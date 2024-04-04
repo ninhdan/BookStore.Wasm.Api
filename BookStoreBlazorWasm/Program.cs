2a3494498ba02ed18b8da165c4c62df90fb59790
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-04-04 18:16:02 +0700

    Update Production Appsetings

--- BookStoreBlazorWasm/Program.cs
+++ BookStoreBlazorWasm/Program.cs
@@ -14,8 +14,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7088") });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5182") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7088") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5182") });
 
 
 builder.Services.AddBlazoredModal();
