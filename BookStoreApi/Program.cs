e844895a1e6e6d82533b0ca938adb1444e825237
Nguyen Hoa Ninh Dan <dh52007049@student.stu.edu.vn>
2024-04-04 18:25:24 +0700

    update program.cs

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -64,11 +64,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 
 app.UseCors(policy =>
